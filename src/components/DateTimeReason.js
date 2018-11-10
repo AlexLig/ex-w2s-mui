edc80ecfa8aaee5978932f73f01716feab4a56a9
KostasLib <libekonst@gmail.com>
2018-11-11 01:19:34 +0200

    Implement Input masks.

--- src/components/DateTimeReason.js
+++ src/components/DateTimeReason.js
@@ -1,33 +1,82 @@
-import React, {Fragment} from 'react'
-
+import React, { Fragment } from 'react'
 import { Button, TextField, MenuItem } from '@material-ui/core'
+import NumberFormat from 'react-number-format'
+
+function DateFormat(props) {
+  const {onChange, ...other } = props
+
+  return (
+    <NumberFormat
+      {...other}
+      format="##/##/20##"
+      placeholder="ΗΗ/ΜΜ/ΕΕΕΕ"
+      mask={'_'}
+      onValueChange={values => {
+        onChange({
+          target: {
+            value: values.value,
+          },
+        })
+      }}
+    />
+  )
+}
+
+function TimeFormat(props) {
+  const {onChange, ...other } = props
+
+  return (
+    <NumberFormat
+      {...other}
+      format="##:##"
+      placeholder="ΩΩ:ΛΛ"
+
+      mask={'_'}
+      onValueChange={values => {
+        onChange({
+          target: {
+            value: values.value,
+          },
+        })
+      }}
+    />
+  )
+}
 
 const DateTimeReason = props => {
-
   const { reasons, classes, onChange, onAddDateTimeReason } = props
 
   return (
     <Fragment>
       <TextField
-        label="date"
+        label="Ημερομηνία"
         value={props.form.date}
         onChange={onChange('date')}
         margin="normal"
         fullWidth
+        InputProps={{
+          inputComponent: DateFormat,
+        }}
       />
       <TextField
-        label="start"
+        label="Ώρα Έναρξης"
         value={props.form.start}
         onChange={onChange('start')}
         margin="normal"
         fullWidth
+        InputProps={{
+          inputComponent: TimeFormat,
+        }}
       />
       <TextField
-        label="finish"
+        label="Ώρα Λήξης"
         value={props.form.finish}
         onChange={onChange('finish')}
         margin="normal"
         fullWidth
+        InputProps={{
+          inputComponent: TimeFormat,
+        }}
       />
       <TextField
         select
@@ -38,17 +87,16 @@ const DateTimeReason = props => {
         margin="normal"
         // fullWidth
         SelectProps={{
-            MenuProps: {
-              // className: classes.menu,
-              // style: {width: 200}
-            },      
-        }}
-      >
-      {reasons.map(option => (
-        <MenuItem key={option.key} value={option.value} >
-          {option.key}
-        </MenuItem>
-      ))}
+          MenuProps: {
+            // className: classes.menu,
+            // style: {width: 200}
+          },
+        }}>
+        {reasons.map(option => (
+          <MenuItem key={option.key} value={option.value}>
+            {option.key}
+          </MenuItem>
+        ))}
       </TextField>
 
       <Button variant="outlined" onClick={onAddDateTimeReason}>
