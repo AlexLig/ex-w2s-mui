77f01f9ad0825587618c67f3c3b75c7b6832e436
AlexLig <ligkalex@gmail.com>
2018-11-11 22:30:52 +0200

    E8 basic features. WIP

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import E4 from './components/E4'
 import MuiPickersUtilsProvider from 'material-ui-pickers/MuiPickersUtilsProvider'
 import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils'
+import E8 from './components/E8/E8';
 
 class App extends Component {
   render() {
     return (
       // <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <E4 />
+        // <E4 />
+        <E8/>
       // </MuiPickersUtilsProvider>
     )
   }
---
+++ src/components/E8/Overtime.js
@@ -0,0 +1,93 @@
+import React, { Fragment } from 'react'
+import { TextField, InputAdornment, IconButton, FormControl, RadioGroup, FormLabel, FormControlLabel, Radio } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit'
+import NumberFormat from 'react-number-format'
+
+function TimeFormat(props) {
+  const { onChange, ...other } = props
+
+  return (
+    <NumberFormat
+      {...other}
+      format="##:##"
+      placeholder="ΩΩ:ΛΛ"
+
+      mask={'_'}
+      onValueChange={values => {
+        onChange({
+          target: {
+            value: values.value,
+          },
+        })
+      }}
+    />
+  )
+}
+const Overtime = props => {
+  const { onChange,onChangeOvertimeHours, onEdit, onBlur, isDisabled } = props
+
+  return (
+    <Fragment>
+
+      <TextField
+        label="Ώρα Έναρξης"
+        value={props.form.startHour}
+        onChange={onChange('startHour')}
+        onBlur={onBlur('startHour')}
+        margin="normal"
+        fullWidth
+        disabled={isDisabled.startHour}
+        InputProps={{
+          inputComponent: TimeFormat,
+          endAdornment: isDisabled.startHour ? (
+            <InputAdornment variant="filled" position="end">
+              <IconButton
+                onClick={onEdit('startHour')}
+                children={<EditIcon />}
+              />
+            </InputAdornment>
+          ) : null,
+        }}
+      />
+
+      <FormControl component="fieldset" >
+        <FormLabel component="legend">Ώρες υπερωρίας</FormLabel>
+        <RadioGroup
+          aria-label="Gender"
+          name="gender1"
+          value={props.form.overtimeHours}
+          onChange={onChangeOvertimeHours()}
+        >
+          <FormControlLabel value="100" control={<Radio />} label="1 ώρα" />
+          <FormControlLabel value="200" control={<Radio />} label="2 ώρες" />
+          <FormControlLabel value="300" control={<Radio />} label="3 ώρες" />
+          <FormControlLabel value="400" control={<Radio />} label="4 ώρες" />
+          <FormControlLabel value="500" control={<Radio />} label="5 ώρες" />
+        </RadioGroup>
+      </FormControl>
+
+      <TextField
+        label="Ώρα Λήξης"
+        value={props.form.finishHour}
+        onChange={onChange('finishHour')}
+        onBlur={onBlur('finishHour')}
+        margin="normal"
+        fullWidth
+        disabled={isDisabled.finishHour}
+        InputProps={{
+          inputComponent: TimeFormat,
+          endAdornment: isDisabled.finishHour ? (
+            <InputAdornment variant="filled" position="end">
+              <IconButton
+                onClick={onEdit('finishHour')}
+                children={<EditIcon />}
+              />
+            </InputAdornment>
+          ) : null,
+        }}
+      />
+    </Fragment>
+  )
+}
+
+export default Overtime
